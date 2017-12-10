0640f78a4142e497295687f24ec2ab6d4261cf3b
islammohsen <www.islammohsen@gmail.com>
2017-12-10 20:03:58 +0200

    Tweaks

--- HR Managment System/HR Managment System/FileOperation.cs
+++ HR Managment System/HR Managment System/FileOperation.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace HR_Managment_System
 {
@@ -15,6 +16,20 @@ namespace HR_Managment_System
         void read()
         {
 
+        }
+
+        public static bool write(Employee emp)
+        {
+            FileStream Fs = new FileStream("Employee.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter st = new StreamWriter(Fs);
+            string id = emp.Id.ToString(), name = emp.Name, DepId = emp.DepId.ToString(),
+                hireDate = emp.HireDate.ToString();
+            if(id.Length <= 5 && name.Length <= 20 &&  hireDate.Length <= 10 && DepId.Length <= 5)
+            {
+
+                return true;
+            }
+            return false;
         }
     }
 }
