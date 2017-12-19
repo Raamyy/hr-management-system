3c8f2005c9d9d3861771902372df1ce164344910
islammohsen <www.islammohsen@gmail.com>
2017-12-19 17:13:35 +0200

    Add get offset

--- HR Managment System/HR Managment System/FileOperation.cs
+++ HR Managment System/HR Managment System/FileOperation.cs
@@ -19,9 +19,77 @@ namespace HR_Managment_System
         {
 
         }
-        void read()
-        {
 
+
+
+        List<Employee> read() //returns list of all employees in the file.
+        {
+            if (File.Exists("employees.txt"))
+            {
+
+                FileStream FS = new FileStream("Employees.txt", FileMode.Open);
+                StreamReader sr = new StreamReader(FS);
+                List<Employee> Employees = new List<Employee>();
+            
+                while (sr.Peek() != -1)
+                {
+                    Employee temp = new Employee();
+                    char[] id = new char[5];
+                    char[] name = new char[20];
+                    char[] date = new char[10];
+                    char[] depID = new char[5];
+
+                    sr.Read(id, 0, 5);
+                    sr.Read(name, 0, 20);
+                    sr.Read(date, 0, 10);
+                    sr.Read(depID, 0, 5);
+
+                    string Id = new string(id);
+                    string Name = new string(name);
+                    string Date = new string(date);
+                    string DepId = new string(depID);
+
+                    string[] DateInfo = Date.Split('/');
+                    DateTime HireDate = new DateTime(int.Parse(DateInfo[2]), int.Parse(DateInfo[1]), int.Parse(DateInfo[0]));
+
+                    temp.Id = int.Parse(Id);
+                    temp.Name = Name;
+                    temp.HireDate = HireDate;
+                    temp.DepId = int.Parse(DepId);
+
+                    Employees.Add(temp);
+                }
+
+                sr.Close();
+            return Employees;
+            }
+
+            return null;
+        }
+
+
+        List<Employee> GetByDepId(int DepId) //returns list of employees of dep depId
+        {
+            List<Employee> AllEmployees = read();
+            List<Employee> Emp = new List<Employee>();
+            for(int i=0; i<AllEmployees.Count; i++)
+            {
+                if (AllEmployees[i].DepId == DepId)
+                    Emp.Add(AllEmployees[i]);
+            }
+            return Emp;
+        }
+
+        List<Employee> GetByEmpId(int Id) //returns list of employees of dep depId
+        {
+            List<Employee> AllEmployees = read();
+            List<Employee> Emp = new List<Employee>();
+            for (int i = 0; i < AllEmployees.Count; i++)
+            {
+                if (AllEmployees[i].Id == Id)
+                    Emp.Add(AllEmployees[i]);
+            }
+            return Emp;
         }
 
         public static bool writeEmployee(Employee emp, int offset)
@@ -91,6 +159,12 @@ namespace HR_Managment_System
                 return true;
             }
             return false;
+        }
+
+        public static long getOffset()
+        {
+            FileStream Fs = new FileStream("Employees.txt", FileMode.OpenOrCreate);
+            return Fs.Length;
         }
     }
 }
